66766a96375dd9b262d1956457a76b337c46e856
Santiago Orozco <everardoorozcocarmona@gmail.com>
2022-11-17 18:54:21 -0500

    se actualizo la base de datos

--- db/db_tienda_ciclista.sql
+++ db/db_tienda_ciclista.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-11-2022 a las 20:01:52
+-- Tiempo de generación: 18-11-2022 a las 00:53:02
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `info_clientes` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(50) DEFAULT NULL,
+  `nombrec` varchar(50) DEFAULT NULL,
   `ciudad` varchar(50) DEFAULT NULL,
   `direccion` varchar(50) DEFAULT NULL,
   `telefono` bigint(50) DEFAULT NULL,
@@ -42,10 +42,10 @@ CREATE TABLE `info_clientes` (
 -- Volcado de datos para la tabla `info_clientes`
 --
 
-INSERT INTO `info_clientes` (`id`, `nombre`, `ciudad`, `direccion`, `telefono`, `usuario`, `password`, `foto`) VALUES
-(3, 'Madelyn', NULL, 'call12', 3016432743, 'montezgomezdayanna@gmail.com', 'ac87c1ffd2d73c7708fb81e0cbca25186a33832b', 'imgs/clientes/dayanna1_20220902_1846_20220917_1421_20220920_1747.jpeg'),
-(4, 'Jasbleidy', NULL, '', 0, '', 'da39a3ee5e6b4b0d3255bfef95601890afd80709', ''),
-(5, 'Madelyn', 'manizales', 'cll23 crr 25 -20', 322, 'evearsgs@gmail.com', 'eeb8f1dbcd3eedab645d5ab893d199d494579bf3', 'imgs/clientes/Mazda1_20221109_1128.jpg');
+INSERT INTO `info_clientes` (`id`, `nombrec`, `ciudad`, `direccion`, `telefono`, `usuario`, `password`, `foto`) VALUES
+(4, 'Jasbleidy', 'Pereira', 'cll 12', 3125005599, 'jasbleidy@gmail.com', 'da39a3ee5e6b4b0d3255bfef95601890afd80709', ''),
+(5, 'Madelyn', 'manizales', 'cll23 crr 25 -20', 322, 'evearsgs@gmail.com', 'eeb8f1dbcd3eedab645d5ab893d199d494579bf3', 'imgs/clientes/Mazda1_20221109_1128.jpg'),
+(7, 'Juan Camilo Orozco Carmona', 'Ciénaga Magdalena', 'Calle 18c1 Kr36a25', 3108416583, 'jorozcocarmona87@gmail.com', 'f91804887f3f6da4a49a789670038e6af6214a2b', '');
 
 -- --------------------------------------------------------
 
@@ -61,7 +61,7 @@ CREATE TABLE `info_productos` (
   `descripcioncorta` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
   `cantidad` int(11) NOT NULL,
   `valorcomercial` double(22,0) NOT NULL,
-  `foto` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `foto1` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `foto2` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `foto3` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `foto4` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
@@ -74,8 +74,8 @@ CREATE TABLE `info_productos` (
 -- Volcado de datos para la tabla `info_productos`
 --
 
-INSERT INTO `info_productos` (`cod`, `nombre`, `referencia`, `descripcion`, `descripcioncorta`, `cantidad`, `valorcomercial`, `foto`, `foto2`, `foto3`, `foto4`, `foto5`, `fecha`, `hora`) VALUES
-(6, 'Prince', '0001', 'PURA EMOCIÓN\r\nLos diseñadores e ingenieros de Pinarello, han trasladado la increíble experiencia que genera el modelo DOGMA F12 a la nueva PRINCE.\r\nEl resultado es una bicicleta con características técnicas y estándares de rendimiento que superan a muchas de las bicicletas de carretera de alta gama del mercado actual.\r\nEl modelo PRINCE hereda el sistema de integración total de cables de la DOGMA F12, llamado TiCR, que permite una ventaja aerodinámica importante.', 'Descripcion corta', 50, 20000000, 'imgs/productos/prince_20220902_1725_20220927_0901_20220927_1612.jpg', 'imgs/productos/Prince2_20220927_1612.jpg', 'imgs/productos/Prince3_20220927_1613.jpg', 'imgs/productos/Prince4_20220927_1613.jpg', 'imgs/productos/Prince5_20220927_1613.jpg', '2022-09-17', '20:25:11'),
+INSERT INTO `info_productos` (`cod`, `nombre`, `referencia`, `descripcion`, `descripcioncorta`, `cantidad`, `valorcomercial`, `foto1`, `foto2`, `foto3`, `foto4`, `foto5`, `fecha`, `hora`) VALUES
+(6, 'Prince', '0001', 'PURA EMOCIÓN\r\nLos diseñadores e ingenieros de Pinarello, han trasladado la increíble experiencia que genera el modelo DOGMA F12 a la nueva PRINCE.\r\nEl resultado es una bicicleta con características técnicas y estándares de rendimiento que superan a muchas de las bicicletas de carretera de alta gama del mercado actual.\r\nEl modelo PRINCE hereda el sistema de integración total de cables de la DOGMA F12, llamado TiCR, que permite una ventaja aerodinámica importante.', 'Hola', 50, 20000000, 'imgs/productos/prince_20221113_1458.jpg', 'imgs/productos/Prince2_20221113_1458.jpg', 'imgs/productos/Prince3_20221113_1458.jpg', 'imgs/productos/Prince4_20221113_1458.png', 'imgs/productos/Prince5_20221113_1458.jpg', '2022-09-17', '20:25:11'),
 (7, 'Gravel', '002', 'Si quieres ganar una carrera de Gravel, esta es tu bicicleta. Esta es la DOGMA todoterreno.\r\nLa polivalente GREVIL F está preparada para todo. Se adapta a cualquier tipo de combinación de ruedas o neumáticos, adaptándose a todas tus necesidades, dándote la libertad de superar todos los límites y de competir contigo mismo. Incluso fuera de la carretera, la aerodinámica cuenta. Cada vatio ahorrado te acerca a la victoria, por lo que el diseño del GREVIL F se centra principalmente en la aerodinámica y la rigidez. Si quieres triunfar en todos los terrenos, esta es tu bicicleta. Esta es el DOGMA todoterreno.', 'Descripcion corta', 90, 5000000, 'imgs/productos/gravel_20220902_1658_20220927_0902_20220927_1615.jpg', 'imgs/productos/IMG-20200415-WA0106 (1)_20220929_1059.jpg', '', '', '', '2022-09-17', '20:26:10'),
 (8, 'Nytro Dust 3', '003', 'EL MODELO PERFECTO PARA QUIENES BUSCAN UNA NUEVA AVENTURA TODOTERRENO\r\nNYTRO DUST 3, una bicicleta de e-MTB duradera y segura con el auténtico estilo Pinarello. Un modelo que te hará vivir grandes experiencias en todo tipo de senderos. Con su motor Shimano EP8, esta bicicleta te llevará más allá de cualquier obstáculo, haciendo que tus días fuera de la carretera sean inolvidables.\r\nLa bicicleta está equipada con la batería Shimano 630w/h que garantiza una reserva de marcha durante todo el día, ruedas de 29″ y 150 mm de recorrido delantero y trasero.\r\n', 'Descripcion corta', 25, 40000000, 'imgs/productos/Nytro dust3_20220902_1701_20220927_0903_20220927_1539_20220927_1616.jpg', '', '', '', '', '2022-09-17', '20:27:13'),
 (9, 'NYTRO urbanist', '004', 'LA OPCIÓN IDEAL PARA MOVERTE POR TU CIUDAD\r\nLa geometría y la calidad del cuadro, hacen de la NYTRO URBANIST un modelo para uso urbano con un carácter deportivo que la diferencia.\r\nGracias a su bajo peso puedes cargarla fácilmente en tu vehículo y llevarla a la oficina. Además, el cuadro ha sido diseñado para cualquier tipo de conducción lo que hace de ella una bicicleta heterogénea.', 'Descripcion corta', 85, 17000000, 'imgs/productos/nytro urbanist_20220902_1704_20220927_0903_20220927_1616.jpg', '', '', '', '', '2022-09-17', '20:28:32'),
@@ -105,11 +105,10 @@ CREATE TABLE `info_usuarios` (
 
 INSERT INTO `info_usuarios` (`id`, `nombre`, `telefono`, `direccion`, `usuario`, `password`, `foto`) VALUES
 (1, 'Dayanna', 3016432743, 'la floresta', 'montezgomezdayanna@gmail.com', 'cd75a226131c769ac97ae3728cbcd6ebd8b9ad65', 'imgs/usuarios/dayanna1_20220902_1846_20220920_1751.jpeg'),
-(2, 'santiago', 3125005599, 'cll 18', 'everardoorozcocarmona@gmail.com', '2053d551f2308e88a59faaee97b0c5d07d52a597', 'imgs/usuarios/Imagen de WhatsApp 2022-09-17 a las 20_20220920_1753.jpg'),
+(2, 'santiago', 3125005599, 'cll 18', 'everardoorozcocarmona@gmail.com', '2053d551f2308e88a59faaee97b0c5d07d52a597', 'imgs/usuarios/Imagen de WhatsApp 2022-09-17 a las 20_20221111_1201.jpg'),
 (3, 'Robert Lopez', 3192565125, 'Mar De Plata', 'robertlopezoviedo03@gmail.com', 'f3c384d143264ac01b447d38cf19543ce0774b10', 'imgs/usuarios/robert_20220902_1846_20220920_1751.jpeg'),
-(4, 'Juan Camilo', 3108418794, 'cll 18', 'jorozcocarmona87@gmail.com', '1741cbea9e85753063f89b2acae4b4575ca7b615', 'imgs/usuarios/Juan_20220902_1551_20220920_1752.jpg'),
-(7, 'Ingrid', 3045354047, 'calle 21 cra 5', 'ingridcarolinamontanoguerrero@gmail.com', '45041b6f01ae9b1a13e8670fa2d05a9198e26acb', 'imgs/usuarios/WhatsApp Image 2022-09-02 at 6_20220902_1841_20220920_1752.jpeg'),
-(8, 'Breiner', 3012946952, 'Santa Ines', 'breiner@gmail.com', '555cb0b0418f28cf4703e4aa5e7a153b1e410366', 'imgs/usuarios/Anotación 2022-09-02 184322_20220902_1843_20220920_1752.png');
+(4, 'Juan Camilo Orozco Carmona', 3108418794, 'cll 18 c1 Kr 36a25', 'jorozcocarmona87@gmail.com', 'f91804887f3f6da4a49a789670038e6af6214a2b', 'imgs/usuarios/juan_20221113_1456.jpg'),
+(7, 'Ingrid', 3045354047, 'calle 21 cra 5', 'ingridcarolinamontanoguerrero@gmail.com', '45041b6f01ae9b1a13e8670fa2d05a9198e26acb', 'imgs/usuarios/WhatsApp Image 2022-09-02 at 6_20220902_1841_20220920_1752.jpeg');
 
 -- --------------------------------------------------------
 
@@ -119,18 +118,21 @@ INSERT INTO `info_usuarios` (`id`, `nombre`, `telefono`, `direccion`, `usuario`,
 
 CREATE TABLE `info_venta` (
   `cod` int(11) NOT NULL,
-  `cliente` varchar(50) NOT NULL,
-  `fk_product` int(11) NOT NULL
+  `cliente` int(11) NOT NULL,
+  `fk_product` int(11) NOT NULL,
+  `fechac` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `info_venta`
 --
 
-INSERT INTO `info_venta` (`cod`, `cliente`, `fk_product`) VALUES
-(1, 'Madelyn', 6),
-(2, 'Santiago', 15),
-(3, 'Dayanna', 15);
+INSERT INTO `info_venta` (`cod`, `cliente`, `fk_product`, `fechac`) VALUES
+(21, 7, 6, '2022-11-13'),
+(22, 7, 6, '2022-11-13'),
+(26, 4, 6, '2022-11-16'),
+(27, 5, 6, '2022-11-16'),
+(28, 5, 6, '2022-11-16');
 
 --
 -- Índices para tablas volcadas
@@ -159,7 +161,8 @@ ALTER TABLE `info_usuarios`
 --
 ALTER TABLE `info_venta`
   ADD PRIMARY KEY (`cod`),
-  ADD KEY `FK_produc` (`fk_product`);
+  ADD KEY `FK_produc` (`fk_product`),
+  ADD KEY `FK_clie` (`cliente`) USING BTREE;
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -169,7 +172,7 @@ ALTER TABLE `info_venta`
 -- AUTO_INCREMENT de la tabla `info_clientes`
 --
 ALTER TABLE `info_clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `info_productos`
@@ -187,7 +190,7 @@ ALTER TABLE `info_usuarios`
 -- AUTO_INCREMENT de la tabla `info_venta`
 --
 ALTER TABLE `info_venta`
-  MODIFY `cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- Restricciones para tablas volcadas
@@ -197,6 +200,7 @@ ALTER TABLE `info_venta`
 -- Filtros para la tabla `info_venta`
 --
 ALTER TABLE `info_venta`
+  ADD CONSTRAINT `FK_clie` FOREIGN KEY (`cliente`) REFERENCES `info_clientes` (`id`),
   ADD CONSTRAINT `FK_produc` FOREIGN KEY (`fk_product`) REFERENCES `info_productos` (`cod`);
 COMMIT;
 
